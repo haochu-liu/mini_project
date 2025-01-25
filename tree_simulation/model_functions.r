08b9a6d46374f37c7c1db940f2598dc3fb98b0f9
haochu-liu <liu_haochu@outlook.com>
2025-01-25 16:23:22 +0000

    change class

--- tree_simulation/model_functions.r
+++ tree_simulation/model_functions.r
@@ -1,5 +1,8 @@
 coale_model <- function(sample_size) {
-  coale_list <- list(n=sample_size, coale_event=list())
+  if (sample_size!=as.integer(sample_size)) {
+    stop("Sample size must be an integer")
+  }
+  coale_list <- list(n=sample_size, items=list(coale_event=list()))
   class(coale_list) <- "human_genealogy"
   return(coale_list)
 }
@@ -7,9 +10,17 @@ coale_model <- function(sample_size) {
 add_recombination <- function(obj, recombination) {
   if (!inherits(obj, "human_genealogy")) {
     stop("Object must be of class 'human_genealogy'")
-  } else {
-    coale_list <- append(append(obj, list(recomb=list(recomb_rate=recombination,
-                                                      recomb_event=list()))))
-    return(obj)
   }
+  obj$items <- append(obj$items, list(recomb_rate=recombination,
+                                      recomb_event=list()))
+  return(obj)
+}
+
+add_mutation <- function(obj, mutation) {
+  if (!inherits(obj, "human_genealogy")) {
+    stop("Object must be of class 'human_genealogy'")
+  }
+  obj$items <- append(obj$items, list(mutation_rate=mutation,
+                                      mutation_event=list()))
+  return(obj)
 }
