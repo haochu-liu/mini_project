fa096a0956bf2e9a70f3ca86a0d13a4db0f0b768
haochu-liu <liu_haochu@outlook.com>
2025-01-25 15:57:42 +0000

    construct as class

--- tree_simulation/model_functions.r
+++ tree_simulation/model_functions.r
@@ -1,8 +1,15 @@
-coale_model <- function(sample_size, recombination = FALSE){
-  coale_list <- list(n = sample_size, coale_event=list())
-  if (recombination) {
-    coale_list <- append(coale_list, list(recomb=list(recomb_rate=recombination, recomb_event=list())))
-  }
-  coale_list
+coale_model <- function(sample_size) {
+  coale_list <- list(n=sample_size, coale_event=list())
+  class(coale_list) <- "human_genealogy"
+  return(coale_list)
 }
-# construct class for the model
+
+add_recombination <- function(obj, recombination) {
+  if (!inherits(obj, "human_genealogy")) {
+    stop("Object must be of class 'human_genealogy'")
+  } else {
+    coale_list <- append(append(obj, list(recomb=list(recomb_rate=recombination,
+                                                      recomb_event=list()))))
+    return(obj)
+  }
+}
