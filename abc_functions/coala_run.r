3a54aea14c0f09890f34bf263c88e0eadcc0a481
haochu-liu <liu_haochu@outlook.com>
2025-01-29 13:42:28 +0000

    beginning of abc

---
+++ abc_functions/coala_run.r
@@ -0,0 +1,19 @@
+library(coala)
+library(abc)
+
+
+sfs <- c(112, 57, 24, 34, 16, 29, 8, 10, 15)
+
+model <- coal_model(10, 50) +
+  feat_mutation(par_prior("theta", runif(1, 1, 5))) +
+  sumstat_sfs()
+sim_data <- simulate(model, nsim = 2000, seed = 17)
+
+# Getting the parameters
+sim_param <- create_abc_param(sim_data, model)
+# Getting the summary statistics
+sim_sumstat <- create_abc_sumstat(sim_data, model)
+
+posterior <- abc(sfs, sim_param, sim_sumstat, 0.05, method = "rejection")
+print(posterior)
+hist(posterior, breaks = 20)
