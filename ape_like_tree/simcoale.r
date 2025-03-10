38186a8393973dba8209395071fa73f68c75879d
haochu-liu <Liu_haochu@outlook.com>
2025-03-10 13:27:32 +0000

    simcoale

--- ape_like_tree/simcoale.r
+++ ape_like_tree/simcoale.r
@@ -1,4 +1,7 @@
 simcoale <- function(n) {
+  if (n!=as.integer(n)) {
+    stop("Sample size must be an integer")
+  }
   n_edge <- 2 * (n-1) # number of edges in a coale tree
   edge <- matrix(NA, nrow=n_edge, ncol=2)
   edge.length <- rep(NA, n_edge)
@@ -13,5 +16,13 @@ simcoale <- function(n) {
     index <- i*2 - 1:0
     edge[index, 2] <- offspring_node
     edge[index, 1] <- next_node
+    edge.length[index] <- t_sum[i] - node.length[offspring_node]
+    node.length[next_node] <- t_sum[i]
+    pool <- c(setdiff(pool, offspring_node), next_node)
+    next_node <- next_node - 1L
   }
+  tree = list(edge=edge, edge.length=edge.length,
+              coale_time=t, sum_time=t_sum[length(t_sum)], n=n)
+  class(tree) <- "simtree"
+  return(tree)
 }
