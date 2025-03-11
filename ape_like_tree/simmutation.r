c292b75b054022ad254a593f18d62f8056ad9343
haochu-liu <Liu_haochu@outlook.com>
2025-03-11 16:11:08 +0000

    simmutation algorithm

---
+++ ape_like_tree/simmutation.r
@@ -0,0 +1,5 @@
+# input: tree, mutation rate, finite or infinite alleles
+# add mutation uniformly to edges
+# store in a dataframe: edge index, position(time), mutation
+# infinite alleles and finite alleles by tracing lineages from root to leaf
+# output: genotype for each leaf allele
