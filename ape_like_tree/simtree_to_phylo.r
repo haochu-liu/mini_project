044c5af5edc90329bd019419b4be1e661d5c15c6
haochu-liu <Liu_haochu@outlook.com>
2025-03-12 15:50:33 +0000

    add labels to simtree

--- ape_like_tree/simtree_to_phylo.r
+++ ape_like_tree/simtree_to_phylo.r
@@ -1,14 +1,15 @@
 # library(ape)
 
 
-simtree_to_phylo <- function(tree) {
+simtree_to_phylo <- function(tree, label=NULL) {
   if (!inherits(tree, "simtree")) {
     stop("Object must be of class 'simtree'")
   }
 
+  if (is.null(label)) {label <- paste0("t", 1:tree$n)}
   # convert the simtree object to phylo object for ape::plot.phylo
   tree_phylo <- list(edge=tree$edge, edge.length=tree$edge.length,
-                     tip.label=paste0("t", 1:tree$n),
+                     tip.label=label,
                      Nnode=as.integer(tree$n - 1))
   class(tree_phylo) <- "phylo"
   # tree_phylo <- ape::reorder.phylo(tree_phylo, order="cladewise")
