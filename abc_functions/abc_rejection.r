02d3afc45e308e563cc0c0c2d2b6916aa7d2fba9
haochu-liu <liu_haochu@outlook.com>
2025-03-16 11:57:31 +0000

    abc_rejection func

--- abc_functions/abc_rejection.r
+++ abc_functions/abc_rejection.r
@@ -1,3 +1,4 @@
+library(matrixStats)
 source("abc_functions/kernel_functions.r")
 
 
@@ -5,10 +6,43 @@ abc_rejection <- function(obs, param, sumstat, tol, kernel,
                           sigma=NULL, log.weight=FALSE) {
 #' input: obs(vector),
 #' param(matrix: n x length(theta)), sumstat(matrix: n x length(s)),
-#' tol(numeric), kernel(string), sigma=NULL(matrix), log=FALSE(bool)
+#' tol(numeric), kernel(string), sigma=NULL(matrix), log.weight=FALSE(bool)
 #' output: weight or log weight given by the abc rejection sampling
 
   weights <- c()
   n <- dim(param)[1]
   if (is.null(sigma)) {sigma=diag(rep(1, length(obs)))}
+  if (kernel=="uniform") {
+    weights <- vapply(1:n,
+                      function(i) uniform_kernel(obs, sumstat[i, ], tol, sigma),
+                      numeric(1))
+    weights <- weights / sum(weights)
+    if (log.weight) {weights <- log(weights)}
+  } else if (kernel=="triangular") {
+    weights <- vapply(1:n,
+                      function(i) triangular_kernel(obs, sumstat[i, ], tol, sigma),
+                      numeric(1))
+    weights <- weights / sum(weights)
+    if (log.weight) {weights <- log(weights)}
+  } else if (kernel=="epanechnikov") {
+    weights <- vapply(1:n,
+                      function(i) epanechnikov_kernel(obs, sumstat[i, ], tol, sigma),
+                      numeric(1))
+    weights <- weights / sum(weights)
+    if (log.weight) {weights <- log(weights)}
+  } else if (kernel=="biweight") {
+    weights <- vapply(1:n,
+                      function(i) biweight_kernel(obs, sumstat[i, ], tol, sigma),
+                      numeric(1))
+    weights <- weights / sum(weights)
+    if (log.weight) {weights <- log(weights)}
+  } else if (kernel=="gaussian") {
+    weights <- vapply(1:n,
+                      function(i) gaussian_kernel(obs, sumstat[i, ], tol, sigma),
+                      numeric(1))
+    weights <- weights - logSumExp(weights)
+    if (!log.weight) {weights <- exp(weights)}
+  } else {stop("No valid kernel")}
+
+  return(weights)
 }
