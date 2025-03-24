98cde65181be6e58e27be148efda6d188f6d0032
haochu-liu <liu_haochu@outlook.com>
2025-03-24 14:58:34 +0000

    mutation num plots

--- ape_like_tree/example_plots.r
+++ ape_like_tree/example_plots.r
@@ -69,9 +69,9 @@ hist(length_vec, probability = TRUE, col="gray", breaks=20,
 lines(x, length_density)
 
 mutation_mean <- c()
-for (n in 2:100) {
+for (n in 3:50) {
   num <- c()
-  for (t in 1000) {
+  for (t in 1:100) {
     tree1 <- simcoale(n)
     mutation_df <- simmutation(tree=tree1, rate=1, model="finite sites", l_seq = 5)
     if (!is.na(mutation_df$mutations$edge_index[1])) {
@@ -81,19 +81,19 @@ for (n in 2:100) {
   mutation_mean <- c(mutation_mean, mean(num))
 }
 
+plot(3:50, mutation_mean, xlab = "n", ylab = "mean of num of mutations")
+lines(3:50, cumsum(1/1:49)[2:49], type = "l", col = "red")
+
+mutation_mean <- c()
+for (n in 2:100) {
+  num <- c()
+  for (t in 1:1000) {
+    length_rate <- (n-1):1 / 2
+    tree_l <- rhypoexp(1, rate=length_rate)
+    num <- c(num, rpois(1, tree_l/2))
+  }
+  mutation_mean <- c(mutation_mean, mean(num))
+}
+
 plot(2:100, mutation_mean, xlab = "n", ylab = "mean of num of mutations")
 lines(2:100, cumsum(1/1:99), type = "l", col = "red")
-
-# mutation_mean <- c()
-# for (n in 2:100) {
-#   num <- c()
-#   for (t in 1000) {
-#     length_rate <- (n-1):1 / 2
-#     tree_l <- rhypoexp(1, rate=length_rate)
-#     num <- c(num, rpois(1, tree_l/2))
-#   }
-#   mutation_mean <- c(mutation_mean, mean(num))
-# }
-
-# plot(2:100, mutation_mean, xlab = "n", ylab = "mean of num of mutations")
-# lines(2:100, cumsum(1/1:99), type = "l", col = "red")
