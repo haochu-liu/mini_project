24a24a6be5514e77a588d2631df3dfc4af9f92d4
haochu-liu <Liu_haochu@outlook.com>
2025-05-20 16:25:29 +0100

    plot graph

--- sim_gene/test.r
+++ sim_gene/test.r
@@ -1,6 +1,11 @@
 library(ivs)
 library(tibble)
 library(dplyr)
+library(igraph)
 source("sim_gene/sim_ARG.r")
 
-tree1 <- sim_ARG(5, 3)
+tree1 <- sim_ARG(5, 1)
+tree1_matrix <- as.matrix(tree1$edge[, c(1, 2)])
+g <- graph_from_edgelist(tree1_matrix, directed = FALSE)
+g <- delete_vertices(g, V(g)[degree(g) == 0])
+plot(g)
