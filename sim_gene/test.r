1a97033e15995f5f34b6b5fc815cb7bed93e9074
haochu-liu <Liu_haochu@outlook.com>
2025-05-21 14:22:46 +0100

    add test

--- sim_gene/test.r
+++ sim_gene/test.r
@@ -6,11 +6,11 @@ source("sim_gene/sim_ARG.r")
 source("sim_gene/sim_mutation.r")
 
 
-set.seed(100)
+set.seed(10)
 tree <- sim_ARG(5, 1)
 tree_matrix <- as.matrix(tree$edge[, c(1, 2)])
 g <- graph_from_edgelist(tree_matrix, directed = FALSE)
 g <- delete_vertices(g, V(g)[degree(g) == 0])
 plot(g)
 
-tree_mutation <- sim_mutation(tree, 1)
+tree_mutation <- sim_mutation(tree, 2)
