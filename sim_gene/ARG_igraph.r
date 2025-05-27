ff7f647aa1783ce48a13832458c286beab6409e7
haochu-liu <liu_haochu@outlook.com>
2025-05-27 20:50:07 +0100

    move and delete files

--- sim_gene/ARG_igraph.r
+++ sim_gene/ARG_igraph.r
@@ -22,25 +22,22 @@ ARG_igraph <- function(ARG) {
 
   # add node coordinate to layout matrix
   for (i in (ARG$n+1):n) {
-    target_node <- ARG$node$index[i]
+    target_node <- i
     if (target_node %in% recomb_parents) {
       # if the target node is from recombination
       if (is.na(layout_coord[i, 1])) {
         base_node <- ARG$edge$node2[which(target_node == ARG$edge$node1)]
-        base_node_index <- which(base_node == ARG$node$index)
         target_node <- ARG$edge$node1[ARG$edge$node2 %in% base_node] # get all two nodes
-        target_node_index <- which(ARG$node$index %in% target_node)
-        layout_coord[target_node_index, 1] <- c(-1, 1) + layout_coord[base_node_index, 1]
-        layout_coord[target_node_index, 2] <- current_level
+        layout_coord[target_node, 1] <- c(-1, 1) + layout_coord[base_node, 1]
+        layout_coord[target_node, 2] <- current_level
         current_level <- current_level + 1
       }
     } else {
       # if the target node is from coalescent
       base_node <- ARG$edge$node2[which(target_node == ARG$edge$node1)]
-      base_node_index <- which(ARG$node$index %in% base_node)
-      target_node_index <- which(ARG$node$index %in% target_node)
-      layout_coord[target_node_index, 1] <- mean(layout_coord[base_node_index, 1])
-      layout_coord[target_node_index, 2] <- current_level
+      target_node <- which(ARG$node$index %in% target_node)
+      layout_coord[target_node, 1] <- mean(layout_coord[base_node, 1])
+      layout_coord[target_node, 2] <- current_level
       current_level <- current_level + 1
     }
   }
