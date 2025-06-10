991cac1400bdc0a7a1dc1bc8ffafda434223d6a5
haochu-liu <liu_haochu@outlook.com>
2025-06-10 14:09:45 +0100

    effective_R

--- sim_gene/effective_R.r
+++ sim_gene/effective_R.r
@@ -10,7 +10,7 @@ effective_R <- function(mat, delta, L, rho) {
 
   if (sum(mat) == L) {
     # if contain full material
-    R_eff <- rho * (1-(1-1/delta)^(L-1)) / 2
+    R_eff <- rho * (1-(1-1/delta)^(L-1))
     return(list(R_eff = R_eff,
                 probstartcum=cumsum(rep(1/L, L))))
   } else {
@@ -18,5 +18,19 @@ effective_R <- function(mat, delta, L, rho) {
     v_s <- c(1:L)[mat & (mat != c(0, mat[1:(L-1)]))] # compute s1, ..., sb
     v_e <- c(1:L)[mat & (mat != c(mat[2:L], 0))]     # compute e1, ..., eb
     v_e <- c(0, v_e)                                 # add e0
+    b <- length(v_s)                                 # number of blocks
+
+    # compute R_eff
+    R_gap <- R * delta * (1 - (1 - 1/delta)^(v_s - v_e[1:b]))
+    R_eff <- sum(R_gap) + R * (sum(mat) - b)
+    R_eff <- R_eff - sum(R_gap * (1 - 1/delta)^(L - (v_s - v_e[1:b]))) -
+      R * (1 - 1/delta)^(L - 1) * (sum(mat) - b)
+
+    # generate probstart
+    probstart <- rep(R * (1 - (1 - 1/delta)^(L - 1)), L)
+    probstart[v_s] <- R_gap * (1 - (1 - 1/delta)^(L - (v_s - v_e[1:b])))
+
+    return(list(R_eff = R_eff,
+                probstartcum=cumsum(probstart)))
   }
 }
